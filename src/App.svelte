41b6f2bc1b101b3a1e27603889203481956b22f2
Nathan Griffiths <ngriffiths@postmedia.com>
2024-07-20 17:01:00 -0700

    initial currentShips component

--- src/App.svelte
+++ src/App.svelte
@@ -3,14 +3,16 @@
     import { onMount } from 'svelte';
     import { csvParse } from 'd3-dsv';
     import Topline from "$components/Topline.svelte";
+    import CurrentShips from "$components/CurrentShips.svelte";
 
     // DATA
+    const currentShipsUrl = 'https://raw.githubusercontent.com/vs-postmedia/tanker-tracker/master/data/current-ships.json';
     // import data from "$data/data.js";
     // import { menuItems } from "$data/menu-items";
 
     // VARIABLES
-    let data;
-    let count = 122575;
+    let currentShipsData;
+    let totalShips = 122575;
     const monthlyShips = {
         dimensions: 'width:100%;height:300px;',
         url: 'https://flo.uri.sh/visualisation/18776706/embed'
@@ -19,16 +21,22 @@
     // REACTIVE VARIABLES
     // $: value, updateData(value);
 
-    async function fetchData(url) {
-        const resp = await fetch(url);
-        data = await resp.text();
-        return csvParse(data);
+    async function fetchData(url, format) {
+        let parsedData;
+        try {
+            const resp = await fetch(url);
+            const data = await resp.text();
+            currentShipsData = format === 'json' ? JSON.parse(data) : csvParse(data);
+        } catch (err) {
+            console.error('Error: ', error)
+        }
+
     }
 
     async function init() {
         // fetch remote data
-        data = await fetchData(dataUrl);
-        console.log(data);
+        await fetchData(currentShipsUrl, 'json');
+        // console.log(currentShipsData);
     }
 
     onMount(init);
@@ -37,12 +45,17 @@
 <main>
     <!-- this can't load until  -->
     <Topline 
-        count={count}
+        count={totalShips}
     />
 
+    <CurrentShips
+        data={currentShipsData}
+    />
 
     <!-- MONTHLY SHIPS -->
     <iframe src={monthlyShips.url} title='Monthly tanker traffic' class='flourish-embed-iframe' frameborder='0' scrolling='no' style={monthlyShips.dimensions} sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/18627199/?utm_source=embed&utm_campaign=visualisation/18627199' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>
+
+
 </main>
 
 <footer>
---
+++ src/components/CurrentShips.svelte
@@ -0,0 +1,44 @@
+<script>
+    export let data;
+    
+    // LIBS
+    import { onMount } from 'svelte';
+
+    // VARS
+    let count;
+    $: is_are = count === 1 ? 'is' : 'are';
+    $: tanker = count === 1 ? 'tanker' : 'tankers';
+    $: count = data ? data.length : null;
+    $: console.log(count, is_are, tanker);
+
+    // FUNCTIONS
+    function init() {
+        console.log('CURRENT SHIPS INIT!')   
+    }
+
+    // LIGHTS! CAMERA! ACTION!
+    onMount(init);
+</script>
+
+<div id="current-ships">
+    <p>There {is_are} currently {count} oil tankers moored at the Westridge and Suncore terminals in the Burrard Inlet.</p>
+</div>
+
+
+<style>
+    .topline-text {
+        /* font-family: 'BentonSansCond-Regular' !important; */
+        font-size: 1.4rem;
+        line-height: 1.65rem;
+        margin: 0 auto 1rem auto;
+        max-width: 95%;
+        text-align: center;
+    }
+    .big-num {
+        color: #A62F22;
+        display: block;
+        /* font-family: 'BentonSansCond-Bold' !important; */
+        font-size: 2rem;
+        margin-bottom: 15px;
+    }
+</style>
